33395a3aed82cc83b997a6bc2c2c5ce7ba6b16ba
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2022-11-23 21:29:51 +0900

    lesson4

--- my-wave-portal/scripts/run.ts
+++ my-wave-portal/scripts/run.ts
@@ -1,6 +1,9 @@
 import { ethers } from "hardhat";
 
 const main = async () => {
+  // Hardhatが提供する任意のアドレスを返す関数
+  const [owner, randomPerson] = await ethers.getSigners();
+
   // WavePortalコントラクトがコンパイルされる
   // getContractFactory関数は、デプロイをサポートするライブラリのアドレスとWavePortalコントラクトの連携を行っている
   const waveContractFactory = await ethers.getContractFactory("WavePortal");
@@ -12,7 +15,20 @@ const main = async () => {
   // WavePortalコントラクトが、ローカルのブロックチェーンにデプロイされるまで待つ処理を行っている
   const wavePortal = await waveContract.deployed();
 
-  console.log("WavePortal address: ", wavePortal.address);
+  console.log("WavePortal deployed to: ", wavePortal.address);
+  console.log("WavePortal deployed by: ", owner.address);
+
+  let waveCount = await waveContract.getTotalWaves();
+  let waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
+
+  // ほかのユーザーがあなたに「👋（wave）」を送った状態をシミュレーションしている
+  waveTxn = await waveContract.connect(randomPerson).wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
